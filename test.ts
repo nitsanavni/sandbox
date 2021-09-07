8527c56cb3875abfbb769890a129fe4fc96048df
Nitsan Avni <nitsanav@gmail.com>
2021-09-07 09:06:00 +0000

    ava test get cli param

--- test.ts
+++ test.ts
@@ -2,19 +2,26 @@ import test from "ava";
 import { Subject } from "rxjs";
 import { map, delay, filter, tap, take } from "rxjs/operators";
 
+import meow from "meow";
+
+const {
+  flags: { iterations },
+} = meow({
+  importMeta: import.meta,
+  flags: { iterations: { type: "string", alias: "i", default: "1000" } },
+});
+
 test("infinite loop with rxjs", (t) => {
   t.plan(1);
   const subject = new Subject<number>();
 
   const incr = (i) => ++i;
 
-  const iterations = 1000;
-
   const obs = subject.pipe(
-    filter((i) => i >= iterations),
+    filter((i) => i >= +iterations),
     take(1),
     tap(() => sub.unsubscribe()),
-    tap((i) => t.is(i, iterations))
+    tap((i) => t.is(i, +iterations))
   );
 
   const sub = subject.pipe(map(incr), delay(2)).subscribe(subject);
